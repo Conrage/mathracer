7730250aecd17f0d1d583c4e8e9fa0a6b3d38d04
Conrad-C <jvococonrad@gmail.com>
2021-05-21 11:03:32 -0300

    Animation fix, server joining and creating room

--- index.js
+++ index.js
@@ -1,34 +1,91 @@
-const app = require('express')();
+const app = require("express")();
 
-const httpServer = require('http').Server(app);
+const httpServer = require("http").Server(app);
 
-app.get('/data', (req, res) => {
-    res.sendFile(__dirname + '/data.json');
-})
+const { v4: uuidv4 } = require("uuid");
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-app.get('/images/:img', (req, res) => {
-    res.sendFile(__dirname + '/images/'+req.params.img);
-})
+app.get("/data", (req, res) => {
+  res.sendFile(__dirname + "/data.json");
+});
 
-const io = require('socket.io')(httpServer, {
-    cors:{
-        origin:"*",
-    }
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/images/:img", (req, res) => {
+  res.sendFile(__dirname + "/images/" + req.params.img);
+});
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+  },
 });
 
 const PORT = 3000 || process.env.PORT;
 
-io.on('connect', socket => {
-    console.log('User connected')
+let rooms = [];
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected')
-    })
+io.on("connect", (socket) => {
+  console.log("User connected");
+
+  socket.on("join-room", (payload) => {
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].code === payload.code) {
+        if (rooms[i].users.length < 4) {
+          rooms[i].users.push({
+            nick: payload.user.nick ? payload.user.nick : "Guest",
+            color: payload.user.color,
+            id: socket.id,
+          });
+          socket.emit("authorized-" + rooms[i].code, true);
+          return;
+        }
+      }
+    }
+    rooms.push({
+      code: payload.code,
+      owner: socket.id,
+      users: [
+        {
+          nick: payload.user.nick ? payload.user.nick : "Guest",
+          color: payload.user.color,
+          id: socket.id,
+        },
+      ],
+    });
+  });
+  socket.on("create-room", (payload) => {
+    let code = uuidv4().substring(0, 6);
+    rooms.push({
+      code: code,
+      owner: socket.id,
+      users: [
+        {
+          nick: payload.user.nick ? payload.user.nick : "Guest",
+          color: payload.user.color,
+          id: socket.id,
+        },
+      ],
+    });
+    socket.emit("authorized", code);
+  });
+
+  socket.on("enter-room", (payload) => {
+    const room = rooms.find((element) => element.code == payload);
+
+    const roomInfo = {
+      room: room,
+      yourInfo: room.users.find((element = element.id == socket.id)),
+    };
+
+    socket.emit("room-info", roomInfo);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
-httpServer.listen(PORT, ()=>{
-    console.log('Listening to ' + PORT)
-})
+httpServer.listen(PORT, () => {
+  console.log("Listening to " + PORT);
+});
