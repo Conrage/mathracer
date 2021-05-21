031c2ce8f143dc9d2b1c3d38d6cb79150e107724
Conrad-C <jvococonrad@gmail.com>
2021-05-21 08:46:36 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+
+const httpServer = require('http').Server(app);
+
+app.get('/data', (req, res) => {
+    res.sendFile(__dirname + '/data.json');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/images/:img', (req, res) => {
+    res.sendFile(__dirname + '/images/'+req.params.img);
+})
+
+const io = require('socket.io')(httpServer, {
+    cors:{
+        origin:"*",
+    }
+});
+
+const PORT = 3000 || process.env.PORT;
+
+io.on('connect', socket => {
+    console.log('User connected')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+});
+
+httpServer.listen(PORT, ()=>{
+    console.log('Listening to ' + PORT)
+})
